8b0d2402b91976e843c0c1b21f1987f2ab32a298
Shreya Pramanik <100830699+ShreyaPramanik47@users.noreply.github.com>
2023-01-04 13:07:22 +0530

    Create Reverse_Integer.cpp

---
+++ Medium/Reverse_Integer.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int num=x;
+        long long ans=0;
+      while(num!=0){
+         int digit = num%10;
+          ans = (ans*10)+digit;
+          if(ans>=INT_MAX || ans<=INT_MIN){
+              return 0;
+          }
+          num=num/10;
+
+      }  
+      return ans;
+    }
+};
+
+// Problem Link: https://leetcode.com/problems/reverse-integer/description/  
